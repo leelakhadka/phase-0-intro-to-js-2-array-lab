2dfb8ee9905d4c88df8d2cf37937cfeeb12a2369
leela khadka <khadkaleela3@gmail.com>
2022-07-23 15:58:35 -0700

    adding all passed tests

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(catName) {
+    return cats.push(catName);
+}
+
+function destructivelyPrependCat(catName) {
+    return cats.unshift(catName);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(catName) {
+    const copyOfCats = [...cats];
+    copyOfCats.push(catName);
+    return copyOfCats;
+}
+
+
+function prependCat(catName) {
+    const copyOfCats = [...cats];
+    copyOfCats.unshift(catName);
+    return copyOfCats;
+}
+
+function removeLastCat(catName) {
+    const copyOfCats = [...cats];
+    copyOfCats.pop(catName);
+    return copyOfCats;
+}
+
+
+function removeFirstCat(catName) {
+    const copyOfCats = [...cats];
+    copyOfCats.shift(catName);
+    return copyOfCats;
+}
